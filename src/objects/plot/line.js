f580f2710bfc74d56e306922f55d7e72e0382af3
Vuk <vukmilicic@gmail.com>
2014-08-14 18:03:10 +0200

    updateTooltipPosition is identity function if series.updateTooltipPosition is not defined

--- src/objects/plot/line.js
+++ src/objects/plot/line.js
@@ -38,6 +38,7 @@
                 updated,
                 removed,
                 orderedSeriesArray,
+                updateTooltipPosition,
 
                 setActiveLine = function(id) {
                     d3.selectAll('path.dimple-line').classed('active', false)
@@ -118,7 +119,7 @@
                             leaveData.chart  = chart;
                             leaveData.series = series;
                         } else {
-                            series.updateTooltipPosition(d3.mouse(this));
+                            updateTooltipPosition(d3.mouse(this));
                         }
                     }
                 },
@@ -212,6 +213,12 @@
                         series.setTimePoint(leaveData.data.lana['time.interval']);
                     }
                 };
+
+            if (series.updateTooltipPosition) {
+                updateTooltipPosition = series.updateTooltipPosition.bind(series);
+            } else {
+                updateTooltipPosition = function() {};
+            }
 
             // Handle the special interpolation handling for step
             interpolation =  (series.interpolation === "step" ? "step-after" : series.interpolation);
