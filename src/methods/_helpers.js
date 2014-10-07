77209a7b985426c2d934b2d6f4ab72b2f56af0f8
Matjaz <matjaz@grosek.si>
2014-10-07 09:15:27 +0200

    wrap function

--- src/methods/_helpers.js
+++ src/methods/_helpers.js
@@ -173,6 +173,32 @@
                 stroke = chart.getColor(d.aggField.slice(-1)[0]).stroke;
             }
             return stroke;
+        },
+
+        // wrap label text
+        // from http://bl.ocks.org/mbostock/7555321
+        wrap: function(text, width) {
+            text.each(function() {
+                var text = d3.select(this),
+                    words = text.text().split(/\s+/).reverse(),
+                    word,
+                    line = [],
+                    lineNumber = 0,
+                    lineHeight = 1.1, // ems
+                    y = text.attr("y"),
+                    dy = parseFloat(text.attr("dy")),
+                    tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+                while (word = words.pop()) {
+                    line.push(word);
+                    tspan.text(line.join(" "));
+                    if (tspan.node().getComputedTextLength() > width) {
+                        line.pop();
+                        tspan.text(line.join(" "));
+                        line = [word];
+                        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                    }
+                }
+            });
         }
 
     };
